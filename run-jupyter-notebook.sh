3800a2d27af0e9845d400982fada147076809937
Joao Figueiredo <joaonvfigueiredo@gmail.com>
2017-07-22 22:47:17 +0100

    fixed docker host mount path

--- run-jupyter-notebook.sh
+++ run-jupyter-notebook.sh
@@ -2,4 +2,4 @@
 
 docker pull ldssa/learning-units
 
-docker run  --volume `pwd`/units:/root/units --workdir /root/units -it --rm -p 127.0.0.1:8888:8888 ldssa/learning-units:latest
+docker run  --volume `pwd`:/root/units --workdir /root/units -it --rm -p 127.0.0.1:8888:8888 ldssa/learning-units:latest
